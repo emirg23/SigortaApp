570b7f8068c0a186f4d22ef8d1606f01bcf28416
Hasan Kaya <kayahasa@itu.edu.tr>
2025-12-03 11:15:59 +0300

    ref

---
+++ Controllers/UtilsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SigortaApp.Extensions;
+
+namespace SigortaApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UtilsController : ControllerBase
+    {
+        public UtilsController() { }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            var number = "12";
+            var converted = int.Parse(number);
+            number.ToInt();
+
+
+            List<string> result = new List<string>();
+
+            if(result != null && result.Count > 0)
+            {
+                // Do something
+            }
+
+            if(result.IsNullOrEmpty())
+            {
+                // Do something
+            }
+        }
+    }
+}
---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace SigortaApp.Extensions
+{
+    public static class StringExtensions
+    {
+        public static int ToInt(this string str)
+        {
+            return int.Parse(str);
+        }
+
+        //IsNullOrEmpty List
+        public static bool IsNullOrEmpty<T>(this List<T> list)
+        {
+            return list == null || list.Count == 0;
+        }
+    }
+}
